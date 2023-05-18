3fc5a6c60e87b019ef968a95b0551280f4e6c48e
Mehedi <developermehedi128@gmail.com>
2023-05-18 16:40:29 +0600

    gallery added

---
+++ src/Pages/Home/Gallery.jsx
@@ -0,0 +1,44 @@
+
+const Gallery = () => {
+    return (
+        <div>
+            <div className="md:w-2/4 mx-auto text-center">
+                <h2 className="font-extrabold text-5xl text-blue-950 my-10"> Our Gallery </h2>
+                <p className="text-xl text-slate-800"> Whether you&rsquo;re searching for the perfect gift or simply want to relive the joy of your own childhood, Toy Wonderland is your one-stop destination. Come, let your imagination soar and experience the magic of play. Start exploring our toy gallery today</p>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:mx-20 mx-5 my-10">
+            <div className="border-4 rounded-xl border-emerald-900 hover:scale-125 transition-all">
+                <img className="rounded-xl" src="https://img.freepik.com/free-photo/cute-little-boy-playing-park_1157-20375.jpg?w=740&t=st=1684405859~exp=1684406459~hmac=b1003b4ea691f542271b35bf5a16cc0c3646bf68205e9d2e1ba75db7108909bf" alt="" />
+            </div>
+            <div className="border-4 rounded-xl border-emerald-900 hover:scale-125 transition-all">
+                <img className="rounded-xl" src="https://img.freepik.com/free-photo/cute-little-boy-playing-park_1157-20375.jpg?w=740&t=st=1684405859~exp=1684406459~hmac=b1003b4ea691f542271b35bf5a16cc0c3646bf68205e9d2e1ba75db7108909bf" alt="" />
+            </div>
+            <div className="border-4 rounded-xl border-emerald-900 hover:scale-125 transition-all">
+                <img className="rounded-xl" src="https://img.freepik.com/free-photo/cute-little-boy-playing-park_1157-20375.jpg?w=740&t=st=1684405859~exp=1684406459~hmac=b1003b4ea691f542271b35bf5a16cc0c3646bf68205e9d2e1ba75db7108909bf" alt="" />
+            </div>
+            <div className="border-4 rounded-xl border-emerald-900 hover:scale-125 transition-all">
+                <img className="rounded-xl" src="https://img.freepik.com/free-photo/cute-little-boy-playing-park_1157-20375.jpg?w=740&t=st=1684405859~exp=1684406459~hmac=b1003b4ea691f542271b35bf5a16cc0c3646bf68205e9d2e1ba75db7108909bf" alt="" />
+            </div>
+            <div className="border-4 rounded-xl border-emerald-900 hover:scale-125 transition-all">
+                <img className="rounded-xl" src="https://img.freepik.com/free-photo/cute-little-boy-playing-park_1157-20375.jpg?w=740&t=st=1684405859~exp=1684406459~hmac=b1003b4ea691f542271b35bf5a16cc0c3646bf68205e9d2e1ba75db7108909bf" alt="" />
+            </div>
+            <div className="border-4 rounded-xl border-emerald-900 hover:scale-125 transition-all">
+                <img className="rounded-xl" src="https://img.freepik.com/free-photo/cute-little-boy-playing-park_1157-20375.jpg?w=740&t=st=1684405859~exp=1684406459~hmac=b1003b4ea691f542271b35bf5a16cc0c3646bf68205e9d2e1ba75db7108909bf" alt="" />
+            </div>
+            <div className="border-4 rounded-xl border-emerald-900 hover:scale-125 transition-all">
+                <img className="rounded-xl" src="https://img.freepik.com/free-photo/cute-little-boy-playing-park_1157-20375.jpg?w=740&t=st=1684405859~exp=1684406459~hmac=b1003b4ea691f542271b35bf5a16cc0c3646bf68205e9d2e1ba75db7108909bf" alt="" />
+            </div>
+            <div className="border-4 rounded-xl border-emerald-900 hover:scale-125 transition-all">
+                <img className="rounded-xl" src="https://img.freepik.com/free-photo/cute-little-boy-playing-park_1157-20375.jpg?w=740&t=st=1684405859~exp=1684406459~hmac=b1003b4ea691f542271b35bf5a16cc0c3646bf68205e9d2e1ba75db7108909bf" alt="" />
+            </div>
+            <div className="border-4 rounded-xl border-emerald-900 hover:scale-125 transition-all">
+                <img className="rounded-xl" src="https://img.freepik.com/free-photo/cute-little-boy-playing-park_1157-20375.jpg?w=740&t=st=1684405859~exp=1684406459~hmac=b1003b4ea691f542271b35bf5a16cc0c3646bf68205e9d2e1ba75db7108909bf" alt="" />
+            </div>
+            
+            </div>
+
+        </div>
+    );
+};
+
+export default Gallery;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../Banner";
+import Gallery from "../Gallery";
 
 const Home = () => {
     return (
         <>
            <Banner/>
+           <Gallery/>
         </>
     );
 };
