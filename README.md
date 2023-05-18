4e3c842a88e8a43c7793ecd2df41381c371cd202
Mehedi <developermehedi128@gmail.com>
2023-05-18 06:39:18 +0600

    starter file

---
+++ README.md
@@ -0,0 +1,2 @@
+# welcome Toy-PLanet
+
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div>
+      <h2 className="text-center text-5xl text-neutral-800">Welcome toy-planet</h2>
+    </div>
+  );
+};
+
+export default App;
